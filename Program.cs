1729377f9891e08a52001045ed09636f803aedc2
Анонимный Аноним <exxxar@users.noreply.github.com>
2018-11-27 00:36:17 +0300

    начало разработки обфускатора под луа файлы

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using System.Net;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+
+namespace Obfuskator
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            if (args.Length <= 0)
+                return;
+
+            if (String.IsNullOrEmpty(args[0].Trim()))
+                return;
+            
+            string folder = args[0];
+
+            if (!Directory.Exists(String.Format("{0}\\backup", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))))
+                Directory.CreateDirectory(String.Format("{0}\\backup", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)));
+            Utils.DirectoryCopy(folder, String.Format("{0}\\backup\\{1}", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), DateTime.Now.Ticks), true);
+
+
+            Directory.EnumerateFileSystemEntries(folder, "*.*", SearchOption.TopDirectoryOnly)
+             .Where(s => Directory.Exists(s))
+             .ToList()
+             .ForEach(el =>
+             {
+                 Console.WriteLine(folder);
+                 string newDir = el.Split('\\')[el.Split('\\').Length - 1];
+                 string newDirName = Utils.CreateMD5(newDir);
+                 Directory.CreateDirectory(folder + "\\" + newDirName);
+                 Utils.DirectoryCopy(el, folder + "\\" + newDirName, true);
+                 if (Directory.Exists(el))
+                     Directory.Delete(el, true);
+
+                 if (Directory.Exists(folder + "\\" + newDirName))
+                     Utils.recurseFolder(folder + "\\" + newDirName);
+             });
+
+
+            Directory.EnumerateFileSystemEntries(folder, "*.*", SearchOption.AllDirectories)
+            .Where(s => s.EndsWith(".lua") || s.EndsWith(".xml"))
+            .ToList()
+            .ForEach(el =>
+            {
+                FileInfo f = new FileInfo(el);
+                if (f.Name.EndsWith(".lua"))
+                {
+                    string localPath = $"{ Path.GetDirectoryName(el) }\\{ Utils.CreateMD5(f.Name.Split('.')[0])}.lua";
+                    f.MoveTo(localPath);
+
+                    Process myProcess = new Process();
+
+                    try
+                    {
+                        Utils.DownloadLuac(localPath);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                if (f.Name == "meta.xml")
+                {
+
+                    bool notWrite = false;
+                    List<string> newMeta = new List<string>();
+                    File.ReadAllLines(el)
+                    .ToList()
+                    .ForEach(str =>
+                    {
+
+                        Regex regex = new Regex(@"src=""(\S+)""");
+                        MatchCollection matches = regex.Matches(str);
+                        if (matches.Count > 0)
+                        {
+                            //foreach (Match match in matches)
+                            string[] src = matches[0].Groups[1].Value.Split('/');
+                            string fileName = src[src.Length - 1].Split('.')[0];
+                            string fileTemp = src[src.Length - 1].Split('.')[1];
+                            if (fileTemp.Equals("lua"))
+                                fileName = Utils.CreateMD5(fileName) + ".luac";
+                            else
+                                fileName += "." + fileTemp;
+
+                            string tmpMD5 = "";
+                            for (int i = 0; i < src.Length - 1; i++)
+                            {
+                                tmpMD5 += Utils.CreateMD5(src[i]) + @"/";
+                            }
+                            tmpMD5 += fileName;
+
+                            tmpMD5 = str.Replace($"{ String.Join("/", src)}", $"{tmpMD5}");
+                            Console.WriteLine(tmpMD5);
+                            newMeta.Add(tmpMD5);
+                        }
+                        else
+                        {
+                            if (str.IndexOf("<!--") == -1 && str.IndexOf("-->") == -1 && !notWrite)
+                                newMeta.Add(str);
+
+                            if (str.IndexOf("<!--") != -1 && str.IndexOf("-->") == -1)
+                                notWrite = true;
+                            if (str.IndexOf("<!--") == -1 && str.IndexOf("-->") != -1)
+                                notWrite = false;
+                        }
+                    });
+                    File.WriteAllLines(f.FullName, newMeta.ToArray());
+                }
+            });
+
+            const string PRIVATE_KEY = "ThePasswordToDecryptAndEncryptTheFile";
+            Directory.EnumerateFileSystemEntries(folder, "*.*", SearchOption.AllDirectories)
+           .Where(s => s.EndsWith(".dff") || s.EndsWith(".txd") || s.EndsWith(".col"))
+           .ToList()
+           .ForEach(item =>
+           {
+               Console.WriteLine(item);
+               Utils.FileEncrypt(item, PRIVATE_KEY,true);
+           });
+
+           Console.ReadLine();
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using System.Net;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+
+namespace Obfuskator
+{
+    public static class Utils
+    {
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                if (!File.Exists(temppath))
+                    file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        public static void recurseFolder(String path)
+        {
+            if (!Directory.Exists(path))
+                return;
+            Directory.EnumerateFileSystemEntries(path, "*.*", SearchOption.TopDirectoryOnly)
+          .Where(s => Directory.Exists(s))
+          .ToList()
+          .ForEach(el =>
+          {
+
+              string newDir = el.Split('\\')[el.Split('\\').Length - 1];
+              string newDirName = CreateMD5(newDir);
+              if (!Directory.Exists(path + "\\" + CreateMD5(el.Split('\\')[el.Split('\\').Length - 1])))
+                  Directory.CreateDirectory(path + "\\" + CreateMD5(el.Split('\\')[el.Split('\\').Length - 1]));
+              DirectoryCopy(el, path + "\\" + newDirName, true);
+              if (Directory.Exists(el))
+                  Directory.Delete(el, true);
+
+
+              if (Directory.Exists(path + "\\" + newDirName))
+                  recurseFolder(path + "\\" + newDirName);
+          });
+        }
+
+        public static async Task<System.IO.Stream> Upload(string actionUrl, string path)
+        {
+            HttpContent compile = new StringContent("1");
+            HttpContent debug = new StringContent("0");
+            HttpContent obfuscate = new StringContent("2");
+
+            HttpContent bytesContent = new ByteArrayContent(File.ReadAllBytes(path));
+
+            using (var client = new HttpClient())
+            using (var formData = new MultipartFormDataContent())
+            {
+                formData.Add(compile, "compile");
+                formData.Add(debug, "debug");
+                formData.Add(obfuscate, "obfuscate");
+
+                formData.Add(bytesContent, "luasource", path);
+
+                var response = await client.PostAsync(actionUrl, formData);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+                return await response.Content.ReadAsStreamAsync();
+            }
+        }
+        public async static void DownloadLuac(string path)
+        {
+            MemoryStream ss = (MemoryStream)await Upload(
+              @"http://luac.mtasa.com/index.php",
+              path
+              );
+
+            using (var fs = new BinaryWriter(new FileStream(path + "c", FileMode.Create)))
+            {
+                byte[] b = new byte[1024];
+                ss.Seek(0, SeekOrigin.Begin);
+                ss.CopyTo(fs.BaseStream);
+                ss.Close();
+            }
+
+
+
+
+
+
+        }
+
+        public static byte[] ReadAllBytes(this BinaryReader reader)
+        {
+            const int bufferSize = 4096;
+            using (var ms = new MemoryStream())
+            {
+                byte[] buffer = new byte[bufferSize];
+                int count;
+                while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+                    ms.Write(buffer, 0, count);
+                return ms.ToArray();
+            }
+
+        }
+
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fille the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+
+        public static void FileEncrypt(string inputFile, string password,bool oneBlock=false)
+        {
+            //http://stackoverflow.com/questions/27645527/aes-encryption-on-large-files
+
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+            //create output file name
+            FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 128;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.None;
+
+            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            AES.Mode = CipherMode.CFB;
+            
+            // write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            if (oneBlock)
+            {
+                FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+                //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+                byte[] buffer = new byte[1024];
+                int read;
+
+                try
+                {
+                    read = fsIn.Read(buffer, 0, buffer.Length);
+                    cs.Write(buffer, 0, read);
+                    while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        //  Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                        fsCrypt.Write(buffer, 0, read);
+                    }
+                    fsIn.Close();
+                   
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                finally
+                {
+                    cs.Close();
+                    fsCrypt.Close();
+                }
+            }
+            else
+            {
+                FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+                //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+                byte[] buffer = new byte[1048576];
+                int read;
+
+                try
+                {
+                    while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        //  Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                        cs.Write(buffer, 0, read);
+                    }
+
+                    // Close up
+                    fsIn.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                finally
+                {
+                    cs.Close();
+                    fsCrypt.Close();
+                }
+            }
+           
+        }
+
+ 
+        public static void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 128;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.None;
+            AES.Mode = CipherMode.CFB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    // Application.DoEvents();
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+
+
+        public static void replaceData(string filePath,int offset1,int length1)
+        {
+            FileStream fs = File.Open(filePath,FileMode.)
+        }
+       
+    }
+}
